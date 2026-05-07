0cf1742ae3a233df72123cf0acf3c7bc70b9f949
Akhil Kolli <ceo@clinivault.org>
2026-05-07 21:33:47 +0530

    feat: smart-fhir keycloak container updates

--- misc/smart-fhir-extensions/start.sh
+++ misc/smart-fhir-extensions/start.sh
@@ -4,9 +4,23 @@
 set -eu
 
 TEMPLATE=/opt/keycloak/data/import-templates/smart-fhir-realm-template.json
-OUTPUT=/opt/keycloak/data/import/smart-realm.json
+OUTPUT=/opt/keycloak/data/import/smart-fhir-realm.json
 
 mkdir -p /opt/keycloak/data/import
 envsubst < "$TEMPLATE" > "$OUTPUT"
+
+# Ensure runtime DB credentials are passed explicitly in production.
+if [ -n "${KC_DB_URL:-}" ]; then
+  set -- "$@" "--db-url=${KC_DB_URL}"
+fi
+if [ -n "${KC_DB_USERNAME:-}" ]; then
+  set -- "$@" "--db-username=${KC_DB_USERNAME}"
+fi
+if [ -n "${KC_DB_PASSWORD:-}" ]; then
+  set -- "$@" "--db-password=${KC_DB_PASSWORD}"
+fi
+if [ -n "${KC_DB_SCHEMA:-}" ]; then
+  set -- "$@" "--db-schema=${KC_DB_SCHEMA}"
+fi
 
 exec /opt/keycloak/bin/kc.sh "$@"
